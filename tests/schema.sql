8a269bf4533e663577a7dd7fdcfc5f80abb1a577
Pepo Viola <pepoviola@gmail.com>
2020-10-30 21:16:23 -0300

    fix ci

---
+++ tests/schema.sql
@@ -0,0 +1,56 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.19
+-- Dumped by pg_dump version 9.6.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: dinos; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE dinos (
+    id uuid NOT NULL,
+    name text,
+    weight integer,
+    diet text
+);
+
+
+ALTER TABLE dinos OWNER TO postgres;
+
+--
+-- Name: dinos dinos_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY dinos
+    ADD CONSTRAINT dinos_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
